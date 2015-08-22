45d9fade6b6955123705a290de8234bb8d8012e9
zero13cool <zero13cool@yandex.ru>
2015-08-23 00:22:19 +0300

    Added core.py and define BaseItem class for simple wrapping mongo collections methods

---
+++ gio/core.py
@@ -0,0 +1,65 @@
+# coding: utf-8
+""" Some common functional.
+"""
+from pymongo import ASCENDING
+
+from app import app
+
+__ALL__ = ('BaseItem',)
+
+
+class BaseItem(object):
+    """ Common mongodb item wrapper.
+    """
+    UNSAFE_ATTRS = ('_id', '_gio_data')
+
+    coll = None
+
+    indexes = None
+
+    def __init__(self, *args, **kwargs):
+        self.repo_name = app.config['GIO_WATCHED_REPO']
+        if self.coll is None:
+            raise NotImplementedError('Item subclasses must define coll attr')
+
+        if self.indexes:
+            self.coll.create_index([(i, ASCENDING) for i in self.indexes])
+
+    def insert(self, item, **kwargs):
+        data = copy.deepcopy(item)
+        self.patch_meta_info(data, kwargs.pop('extra', {}))
+        return self.coll.insert_one(data, **kwargs)
+
+    def find(self, *args, **kwargs):
+        """ Wrapper around .find method for skipping some staff keys.
+        """
+        for item in self.coll.find(*args, **kwargs):
+            yield self._safe(item)
+
+    def get(self, *args, **kwargs):
+        """ Wrapper for getting one element from collection.
+        """
+        return self._safe(self.coll.find_one(*args, **kwargs))
+
+    def drop(self):
+        """ Drop it all
+        """
+        return self.coll.drop()
+
+    def patch_meta_info(self, data, extra):
+        meta_info = {
+            '_gio_data': {
+                'repo': self.repo_name
+            }
+        }
+        meta_info['_gio_data'].update(extra)
+        data.update(meta_info)
+        return data
+
+    def _safe(self, item):
+        """ Hide some staff property.
+        """
+        if not item:
+            return item
+
+        return {k: v for k, v in item.items() if k not in self.UNSAFE_ATTRS}
--- gio/issues/db.py
+++ gio/issues/db.py
@@ -4,7 +4,7 @@ from hashlib import md5
 from datetime import datetime
 
 from app import db, app, signals
-from pymongo import ASCENDING
+from core import BaseItem
 from pymongo.collection import ReturnDocument
 
 __ALL__ = ('Issue', 'Event')
@@ -13,23 +13,12 @@ __ALL__ = ('Issue', 'Event')
 issue_updated = signals.signal('issue-updated')
 
 
-class BaseItemClass(object):
+class BaseItemClass(BaseItem):
     """ Base wrapper for working with collection items.
     """
-    UNSAFE_ATTRS = ('_id', '_gio_data')
-
     coll = None
 
     indexes = None
-
-    def __init__(self, *args, **kwargs):
-        self.repo_name = app.config['GIO_WATCHED_REPO']
-
-        if self.coll is None:
-            raise NotImplementedError('Item subclasses must define coll attr')
-
-        if self.indexes:
-            self.coll.create_index([(i, ASCENDING) for i in self.indexes])
 
     def insert_or_update(self, item, **kwargs):
         """ Wrapper around `collection.find_one_and_update` method.
@@ -40,45 +29,6 @@ class BaseItemClass(object):
         return self.coll.find_one_and_update(
             {'id': item['id']}, {'$set': data},
             upsert=True, return_document=ReturnDocument.BEFORE)
-
-    def insert(self, item, **kwargs):
-        data = copy.deepcopy(item)
-        self.patch_meta_info(data, kwargs.pop('extra', {}))
-        return self.coll.insert_one(data, **kwargs)
-
-    def get(self, *args, **kwargs):
-        """ Wrapper for getting one element from collection.
-        """
-        return self._safe(self.coll.find_one(*args, **kwargs))
-
-    def find(self, *args, **kwargs):
-        """ Wrapper around .find method for skipping some staff keys.
-        """
-        for item in self.coll.find(*args, **kwargs):
-            yield self._safe(item)
-
-    def drop(self):
-        """ Drop it all
-        """
-        return self.coll.drop()
-
-    def patch_meta_info(self, data, extra):
-        meta_info = {
-            '_gio_data': {
-                'repo': self.repo_name
-            }
-        }
-        meta_info['_gio_data'].update(extra)
-        data.update(meta_info)
-        return data
-
-    def _safe(self, item):
-        """ Hide some staff property.
-        """
-        if not item:
-            return item
-
-        return {k: v for k, v in item.items() if k not in self.UNSAFE_ATTRS}
 
 
 class IssueClass(BaseItemClass):
